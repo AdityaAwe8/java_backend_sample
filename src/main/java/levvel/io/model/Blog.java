96286300a2797d98a0244706be449764c82a9ff4
Aditya Mantri <mantriaditya8@gmail.com>
2022-10-05 02:58:14 -0500

    Endava Coding Implementation

--- src/main/java/levvel/io/model/Blog.java
+++ src/main/java/levvel/io/model/Blog.java
@@ -6,6 +6,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class Blog {
@@ -22,4 +24,16 @@ public class Blog {
     String author;
     String title;
     String text;
+    List<Comment> allComments;
+
+    public Blog(String userId)
+    {
+        id = userId;
+    }
+
+    public List<Comment> retrieveAllComments()
+    {
+        return allComments;
+    }
+
 }
